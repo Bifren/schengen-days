fa1b35bf16957773680dc75b59bc26004d5c9e22
Bifren <kalchenko.kirill@gmail.com>
2026-02-18 14:37:57 +0200

    Refactor UI to Figma-like layout with preview and timeline UX

--- sw.js
+++ sw.js
@@ -1,12 +1,18 @@
-const CACHE = "schengen-days-v1";
-const ASSETS = ["./","./index.html","./app.js","./manifest.webmanifest"];
+const CACHE = "schengen-days-v7";
+const ASSETS = ["./", "./index.html", "./design-tokens.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
